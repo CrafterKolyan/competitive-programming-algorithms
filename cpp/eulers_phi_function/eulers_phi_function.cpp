b8cc90f6bc75485839770aa7e7528762fd6cd397
Nikolay Korolev <korolevns98@gmail.com>
2019-03-18 23:15:37 +0300

    Remove isEven in Euler's Phi Function implementation

--- cpp/eulers_phi_function/eulers_phi_function.cpp
+++ cpp/eulers_phi_function/eulers_phi_function.cpp
@@ -6,20 +6,19 @@ IntegerType EulersPhiFunction(IntegerType x) {
     IntegerType result = x;
 
     // Case with prime 2 is considered separately (as an optimization)
-    bool isEven = !(x & 1);
-    while (!(x & 1)) {
-        x >>= 1;
-    }
-    if (isEven) {
+    if (!(x & 1)) {
         result >>= 1;
+        while (!(x & 1)) {
+            x >>= 1;
+        }
     }
 
     for (IntegerType i = IntegerType(3); i * i <= x; i += 2) {
         if (x % i == 0) {
+            result -= result / i;
             do {
                 x /= i;
             } while(x % i == 0);
-            result -= result / i;
         }
     }
     if (x > 1) {
